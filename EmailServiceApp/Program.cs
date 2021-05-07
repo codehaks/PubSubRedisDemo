c34348e1e292af5946802a563a7ccd0283c5e45a
Hakim Ghods <ghods.hakim@hotmail.com>
2021-05-08 00:31:45 +0430

    Fix

--- EmailServiceApp/Program.cs
+++ EmailServiceApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 
 namespace EmailServiceApp
 {
@@ -7,6 +8,15 @@ namespace EmailServiceApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            var pubsub = redis.GetSubscriber();
+            pubsub.Subscribe("codehaks", (channel, message) => MessageAction(message));
+        }
+
+        private static void MessageAction(RedisValue message)
+        {
+            Console.WriteLine(message + " just registered.");
         }
     }
 }
